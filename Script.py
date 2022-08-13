38909b0306c3a73cdec7f8a8e0fa82577fa46343
Hᴜᴍᴇɴ ⛔ <107025096+Humen-Git@users.noreply.github.com>
2022-08-13 22:30:42 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -39,7 +39,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/Cinemaveed)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
