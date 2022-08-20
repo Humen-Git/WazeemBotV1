b22b0a286284f49f2506a4a583e6dfc4efb058be
badbunny444 <106466391+badbunny444@users.noreply.github.com>
2022-08-20 16:34:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/Humen-Git/WazeemBot.git /WazeemBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
 fi
-cd /EvaMaria
+cd /WazeemBot
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting WazeemBot...."
 python3 bot.py
